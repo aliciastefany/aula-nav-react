7e9cc562dc84479f9a4c925451ef820aa8d1ca5a
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-09-16 13:53:41 -0300

    navegação básica

---
+++ projeto-react/src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <div>
+            <h2>Página Inicial</h2>
+            <p>Esse é um projeto de react + vite + react-router</p>
+        </div>
+    )
+}
+
+export default Home
